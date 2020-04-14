cc702075d8e4a85e7e8f805261ac63ab41825ecf
Romain Tello <romain.tello@gmail.com>
2020-04-14 21:30:13 +0200

    Add entities and fixtures

---
+++ api/src/Entity/Visit.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VisitRepository")
+ */
+class Visit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="sentVisits")
+     * @ORM\JoinColumn(name="visitor_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $visitor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="receivedVisits")
+     * @ORM\JoinColumn(name="visited_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $visited;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getVisitor(): ?User
+    {
+        return $this->visitor;
+    }
+
+    public function setVisitor(?User $visitor): self
+    {
+        $this->visitor = $visitor;
+
+        return $this;
+    }
+
+    public function getVisited(): ?User
+    {
+        return $this->visited;
+    }
+
+    public function setVisited(?User $visited): self
+    {
+        $this->visited = $visited;
+
+        return $this;
+    }
+}
